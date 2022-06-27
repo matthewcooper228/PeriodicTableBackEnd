9e126259417e09d9dddf32586f2568646b508b3a
matthewcooper228 <matthewcooper228@gmail.com>
2022-06-27 10:40:17 -0700

    separated logic layers

---
+++ DataLayer/StaticStorage.cs
@@ -0,0 +1,17 @@
+﻿using Models;
+namespace DataLayer;
+public class StaticStorage
+{
+    public static List<Element> Elements {get; set;} = new List<Element>(){
+        new Element {
+            AtomicNumber = 1,
+            Symbol = "H",
+            Name = "hydrogen",
+            ImageURL = "",
+            MolarMass = 1.008,
+            Electronegativity = 2.20,
+            IonizationEnergy = 13.598,
+            AtomicRadius = 0.0
+        }
+    };
+}
--- Models/Element.cs
+++ Models/Element.cs
@@ -1,5 +1,13 @@
 ﻿namespace Models;
-public class Class1
+public class Element
 {
+    public int AtomicNumber {get; set; } = 0;
+    public string Symbol {get; set;} = null!;
+    public string Name {get; set;} = null!;
+    public string ImageURL {get; set;} = null!;
+    public double MolarMass {get; set;} = 0.0;
+    public double Electronegativity {get; set;} = 0.0;
+    public double IonizationEnergy {get; set;} = 0.0;
+    public double AtomicRadius {get; set;} = 0.0;
 
 }
